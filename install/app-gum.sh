abed84ee610eb39bc2ecd2be706d7cab742cca85
Gabriel Henaut <gabriel.henaut.peixoto@outlook.com>
2024-11-02 17:07:32 +0000

    aaaaa

--- install/app-gum.sh
+++ install/app-gum.sh
@@ -1,5 +1,6 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
+echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 GUM_VERSION="0.14.5" # Use known good version
 echo "LINK: https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
 wget -O gum.deb "https://github.com/charmbracelet/gum/releases/download/v0.14.5/gum_0.14.5_amd64.deb"
