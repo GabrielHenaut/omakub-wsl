ea28e09e0b043436f1dbdbb7a6f231a77014326b
Gabriel Henaut <gabriel.henaut.peixoto@outlook.com>
2024-11-02 16:56:54 +0000

    test2

--- install/app-gum.sh
+++ install/app-gum.sh
@@ -2,7 +2,7 @@
 cd /tmp
 GUM_VERSION="0.14.5" # Use known good version
 echo "LINK: https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
-wget -O gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
+wget -O gum.deb "https://github.com/charmbracelet/gum/releases/download/v0.14.5/gum_0.14.5_amd64.deb"
 sudo apt install -y ./gum.deb
 rm gum.deb
 cd -
